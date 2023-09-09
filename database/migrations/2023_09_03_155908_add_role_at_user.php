c5bf41abc2b13b9b5d4830cd23b6961688472def
andisuartika <pt.andisuartika@gmail.com>
2023-09-09 15:36:13 +0800

    register & upload image

--- database/migrations/2023_09_03_155908_add_role_at_user.php
+++ database/migrations/2023_09_03_155908_add_role_at_user.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->after('email')->default('users');
+            $table->string('role')->after('email')->default('user');
         });
     }
 
