055d3f8d31ac3542e788a0a603050b58602a3ff4
andisuartika <pt.andisuartika@gmail.com>
2023-09-10 00:13:14 +0800

    midtrans

---
+++ app/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\OrderItem;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $fillable = ['user_id', 'seller_id', 'number', 'total_price', 'payment_status', 'payment_url', 'delivery_address'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function orderItems(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ProductSeeder::class,
+            OrderSeeder::class,
+            OrderItemSeeder::class,
         ]);
     }
 }
